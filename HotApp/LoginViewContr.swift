833c52a912455020e4473141234d024056d8c40b
Aashish Tyagi <aashish1aug@gmail.com>
2017-02-21 22:29:00 +0530

    Implement Facebook Login

--- HotApp/LoginViewContr.swift
+++ HotApp/LoginViewContr.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class LoginViewContr: UIViewController {
 
@@ -18,6 +20,58 @@ class LoginViewContr: UIViewController {
         //let landingScreen = self.storyboard?.instantiateViewController(withIdentifier: "LandingScreenVwContr")
        // self.navigationController?.pushViewController(landingScreen!, animated: true)
     }
+    
+    
+    @IBAction func fbLoginTapped(_ sender: UIButton) {
+        
+        let loginManager = LoginManager()
+        loginManager.logIn([.publicProfile], viewController: self) { loginResult in
+            
+            switch loginResult {
+            
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User Cancelled login")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+            //print("Access Token is : \(accessToken)")
+                self.getUserData()
+            default: break
+            }
+            
+        }
+        
+    }
+    
+    //1 Get User Details
+    func getUserData()
+    {
+        let params = ["fields" :"email,name,location,link,gender"]
+        let graphRequest = GraphRequest(graphPath: "me", parameters: params)
+        graphRequest.start {
+            (urlResponse, requestResult) in
+            
+            switch requestResult {
+            case .failed(let error):
+                print("error in graph request:", error)
+                break
+            case .success(let graphResponse):
+                if let responseDictionary = graphResponse.dictionaryValue {
+                    print("\nResponse is : \(responseDictionary)")
+                    //print("Email id :\(responseDictionary["email"]!)")
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    @IBAction func gPlusLoginTapped(_ sender: UIButton) {
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
